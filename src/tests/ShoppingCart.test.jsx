b3c3856baeb77b4b2dfcdac16f79722e732e178b
Lawrence Li <lawrenceli3202@gmail.com>
2024-06-11 16:04:25 -0400

    test: implement vitest for shopping cart componenet

--- src/tests/ShoppingCart.test.jsx
+++ src/tests/ShoppingCart.test.jsx
@@ -1,5 +1,6 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
 import ShoppingCart from "../components/ShoppingCart";
 
 describe("ShoppingCart Component", () => {
